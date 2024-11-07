8e3737d2a5bf7aeedebaba902b94be7a0997be79
mbilal2913 <mbilal2913@gmail.com>
2024-11-07 10:10:38 +0000

    fix: dropdown in sidebar in not opening

--- resources/views/components/sidebar/nav-link.blade.php
+++ resources/views/components/sidebar/nav-link.blade.php
@@ -1,5 +1,7 @@
+@props(['href', 'icon', 'label'])
+
 <a href="{{ $href }}" 
-   :class="{'active': request()->is('{{ $href }}')}"
+   :class="{'active': currentPath === '{{ $href }}'}"
    class="nav-link flex items-center px-4 py-2.5 text-sm font-medium text-gray-600 dark:text-gray-300 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 group">
     <svg class="w-5 h-5" :class="{'mr-3': !isSidebarMinimized}" fill="none" stroke="currentColor" viewBox="0 0 24 24">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="{{ $icon }}"/>
