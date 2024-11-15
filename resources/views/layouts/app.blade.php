6fac05057b46ac5f37801f842d941524ae720f4e
Muhammad Bilal Saleem <mbilal2913@gmail.com>
2024-11-15 09:46:53 +0500

    remove cdn's of alpine and tailwind

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,25 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>{{ $title ?? env('APP_NAME', 'HMS') }}</title>
-    
+
     <!-- Styles -->
     @vite('resources/css/app.css')
-    <script src="https://cdn.tailwindcss.com"></script>
-    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     @stack('styles')
-    
+
     <style>
         [x-cloak] { display: none !important; }
-        
+
         .nav-link.active {
             @apply bg-indigo-50 text-indigo-600 border-l-4 border-indigo-600;
         }
         .dark .nav-link.active {
             @apply bg-indigo-900/20 text-indigo-400 border-indigo-400;
         }
-        
+
         /* Preloader CSS */
         #preloader {
             display: flex;
@@ -71,12 +69,12 @@
         </div>
     </div>
 
-    <div x-data="{ 
+    <div x-data="{
         isSidebarOpen: false,
         isSidebarMinimized: localStorage.getItem('sidebarMinimized') === 'true',
 
 
-        
+
         toggleSidebar() {
             this.isSidebarMinimized = !this.isSidebarMinimized;
             localStorage.setItem('sidebarMinimized', this.isSidebarMinimized);
@@ -91,7 +89,7 @@
         <!-- Main Content -->
         <div :class="{'lg:pl-64': !isSidebarMinimized, 'lg:pl-20': isSidebarMinimized}"
              class="container flex-grow px-4 mx-auto transition-all duration-300"
-             id="main-content" 
+             id="main-content"
              style="display: none;">
             <div class="pt-10 lg:pt-0">
                 @hasSection('content')
@@ -102,16 +100,16 @@
                 <x-footer />
             </div>
         </div>
-        
+
     </div>
 
-    
+
 
     <!-- Scripts -->
     @vite('resources/js/app.js')
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-        
+
     <script>
         window.addEventListener('load', function() {
             const preloader = document.getElementById('preloader');
